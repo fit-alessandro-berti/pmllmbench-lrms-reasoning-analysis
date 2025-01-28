a8957dacc7fcf357f71a4a300747e8aef60a0d98
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-01-28 13:09:11 +0100

    XES log creation

---
+++ 04_xes_log_creation.py
@@ -0,0 +1,36 @@
+import os
+import json
+import pm4py
+from pm4py.objects.log.obj import EventLog, Trace, Event
+from datetime import datetime
+
+folder = "prel/final_abstract_steps"
+
+results = [x for x in os.listdir(folder) if x.endswith(".json")]
+
+log = EventLog()
+
+timer = 100000
+
+for file in results:
+    res = json.load(open(os.path.join(folder, file), "r", encoding="utf-8"))
+
+    trace = Trace()
+    trace.attributes["model"] = file.split("_cat")[0]
+    trace.attributes["question"] = file.split("_cat")[1].split(".")[0]
+    trace.attributes["concept:name"] = file
+
+    log.append(trace)
+
+    for el in res:
+        event = Event()
+        event["concept:name"] = el["Name"]
+        if "Text" in el:
+            event["text"] = el["Text"]
+            pass
+        event["time:timestamp"] = datetime.fromtimestamp(timer)
+        timer += 1
+
+        trace.append(event)
+
+pm4py.write_xes(log, "xes/output_"+datetime.now().strftime("%Y%m%d")+".xes")
