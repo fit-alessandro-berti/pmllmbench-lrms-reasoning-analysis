ce1e7a80e09ebaa1dace658fd730a904e06c5c41
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-03-31 14:17:30 +0200

    added renaming script

---
+++ renaming.py
@@ -0,0 +1,29 @@
+import os
+
+
+def do_renaming(base_path, original_name, novel_name):
+    if not os.path.exists(base_path):
+        raise Exception("base_path %s does not exists!" % (base_path))
+
+    files = [x for x in os.listdir(base_path) if x.startswith(original_name)]
+
+    for f in files:
+        original_path = os.path.join(base_path, f)
+        new_path = original_path.replace(original_name, novel_name)
+
+        print(original_path, new_path, os.path.exists(original_path), os.path.exists(new_path))
+        os.rename(original_path, new_path)
+
+
+if __name__ == "__main__":
+    original_name = "qwen-qwq-32b_"
+    novel_name = "qwen-qwq-32b-nostepbystep-lessvalidbacktr_"
+
+    if not original_name.endswith("_"):
+        raise Exception("original_name must terminate with _")
+
+    if not novel_name.endswith("_"):
+        raise Exception("novel_name must terminate with _")
+
+
+do_renaming("prel/final_abstract_steps/", original_name, novel_name)
